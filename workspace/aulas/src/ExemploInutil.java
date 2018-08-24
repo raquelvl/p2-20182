dcc1f7ce59816fee80ccbdfdcb1c48f7bac0b787
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2018-08-24 09:52:46 -0300

    removendo antigos
    codigo incompleto

--- workspace/aulas/src/ExemploInutil.java
+++
@@ -1,6 +0,0 @@
-public class ExemploInutil {
-	public static void main(String[] args) {
-		System.out.println("isso nao faz nada! Inutil!");
-	}
-
-}
--- workspace/aulas/src/aulas/ExemploInutil.java
+++
@@ -1,8 +0,0 @@
-package aulas;
-
-public class ExemploInutil {
-	public static void main(String[] args) {
-		System.out.println("isso nao faz nada! Inutil!");
-	}
-
-}

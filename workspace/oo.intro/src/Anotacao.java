4367f9e1e5acbe78d7dc71773ba0102813afe62c
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2018-08-31 09:15:31 -0300

    intro a OO
    novo projeto onde vão ficar as classes dessa introdução a OO

---
+++ workspace/oo.intro/src/Anotacao.java
@@ -0,0 +1,68 @@
+public class Anotacao {
+	private String data;
+	private String anotacao;
+
+	public Anotacao(String data) {
+		this.data = data;
+	}
+
+	public Anotacao(String data, String anotacao) {
+		this.data = data;
+		this.anotacao = anotacao;
+	}
+
+
+
+	public void setAnotacao(String anotacao) {
+		this.anotacao = anotacao;
+	}
+
+	public void complementaAnotacao(String complemento) {
+		anotacao += complemento;
+	}
+
+
+	public String getData() {
+		return data;
+	}
+
+	public String getAnotacao() {
+		return anotacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((anotacao == null) ? 0 : anotacao.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Anotacao other = (Anotacao) obj;
+		if (anotacao == null) {
+			if (other.anotacao != null)
+				return false;
+		} else if (!anotacao.equals(other.anotacao))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Anotacao [data=" + data + ", anotacao=" + anotacao + "]";
+	}
+
+	public boolean contemPalavra(String palavra) {
+		if(anotacao.contains(palavra))
+			return true;
+		return false;
+	}
+}
---
+++ workspace/oo.intro/src/DiarioDeClasse.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+
+public class DiarioDeClasse {
+	private int numeroDeAulas;
+	private Anotacao[] anotacoes;
+	private int qtdeDeAnotacoes;
+
+	public DiarioDeClasse(int numeroDeAulas) {
+		this.numeroDeAulas = numeroDeAulas;
+		anotacoes = new Anotacao[this.numeroDeAulas];
+	}
+
+	public boolean adicionaAnotacao(String data, String anotacao) {
+		if (qtdeDeAnotacoes == numeroDeAulas) 
+			return false;
+		anotacoes[qtdeDeAnotacoes++] = new Anotacao(data, anotacao);
+		return true;
+	}
+	
+	public int getNumeroDeAulas() {
+		return numeroDeAulas;
+	}
+	
+	public int getQtdeDeAnotacoes() {
+		return qtdeDeAnotacoes;
+	}
+
+	public String recuperaAnotacao(int index) {
+		if(index >= 0 && index < qtdeDeAnotacoes)
+			return anotacoes[index].toString();
+		return "anotacao nao existe.";
+	}
+
+	@Override
+	public String toString() {
+		return "DiarioDeClasse [anotacoes=" + Arrays.toString(anotacoes) + "]";
+	}
+
+	public String[] pesquisaPalavra(String palavra) {
+		String str = "";
+		for (int i = 0; i < getQtdeDeAnotacoes(); i++) {
+			if(anotacoes[i].contemPalavra(palavra)) {
+				str = str.concat(anotacoes[i].toString() + "EOAEOAEOA");
+			}
+		}
+		return str.split("EOAEOAEOA");
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(anotacoes);
+		result = prime * result + numeroDeAulas;
+		result = prime * result + qtdeDeAnotacoes;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DiarioDeClasse other = (DiarioDeClasse) obj;
+		if (!Arrays.equals(anotacoes, other.anotacoes))
+			return false;
+		if (numeroDeAulas != other.numeroDeAulas)
+			return false;
+		if (qtdeDeAnotacoes != other.qtdeDeAnotacoes)
+			return false;
+		return true;
+	}
+	
+	
+}
